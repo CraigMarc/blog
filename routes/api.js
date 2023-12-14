09d4fb21e04f5eb1fb5c7dc6f253f626c420a9aa
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-14 16:53:33 -0500

    added delete and update post

--- routes/api.js
+++ routes/api.js
@@ -11,19 +11,24 @@ const posts_controller = require("../controllers/postsController");
 // GET all messages.
 router.get("/", posts_controller.all_posts_get);
 
+// Get all published posts
+
+router.get("/published", posts_controller.all_published_posts_get);
+
+
 // GET request for sign up. 
-router.get("/:messageId", posts_controller.single_post_get);
+router.get("/:postId", posts_controller.single_post_get);
 
 // POST new message.
 router.post("/", posts_controller.create_post);
 
 
 // DELETE message. 
-router.delete("/:messageId", posts_controller.delete_post);
+router.delete("/:postId", posts_controller.delete_post);
 
 // PUT edit message
 
-router.put("/:messageId", posts_controller.edit_post);
+router.put("/:postId", posts_controller.edit_post);
 
 
 
