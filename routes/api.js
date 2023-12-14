35fbead13b6bc7e9736e8451f0097a42c2627956
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-14 17:09:33 -0500

    added /post to post routes

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ const posts_controller = require("../controllers/postsController");
 /// Catagory ROUTES ///
 
 // GET all messages.
-router.get("/", posts_controller.all_posts_get);
+router.get("/posts/", posts_controller.all_posts_get);
 
 // Get all published posts
 
@@ -17,20 +17,28 @@ router.get("/published", posts_controller.all_published_posts_get);
 
 
 // GET request for sign up. 
-router.get("/:postId", posts_controller.single_post_get);
+router.get("/posts/:postId", posts_controller.single_post_get);
 
 // POST new message.
-router.post("/", posts_controller.create_post);
+router.post("/posts/", posts_controller.create_post);
 
 
 // DELETE message. 
-router.delete("/:postId", posts_controller.delete_post);
+router.delete("/posts/:postId", posts_controller.delete_post);
 
 // PUT edit message
 
-router.put("/:postId", posts_controller.edit_post);
+router.put("/posts/:postId", posts_controller.edit_post);
 
+//get comments for post
 
+//router.get("/", comments_controller.all_comments_get);
+
+//add commment to post
+
+//router.get("/", comments_controller.all_comments_get);
+
+//delete comment
 
 
 
