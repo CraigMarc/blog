3e5d0822eadbdd0e9ad9082d62d7bdbdff3942f0
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-24 15:03:24 -0500

    sanitised route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,7 +10,7 @@ mongoose.set("strictQuery", false)
 // get all posts
 
 exports.all_posts_get = asyncHandler(async (req, res, next) => {
-    
+
 
   try {
     let allPosts = await Posts.find().exec()
@@ -18,21 +18,21 @@ exports.all_posts_get = asyncHandler(async (req, res, next) => {
   } catch (error) {
     res.status(500).json({ message: error });
   }
-  
+
 });
 
 // Get single post.
 exports.single_post_get = asyncHandler(async (req, res, next) => {
-  
- 
+
+
   try {
-   let post = await Posts.findById(req.params.postId).exec()
-   res.status(200).json(post)
- } catch (error) {
-   res.status(500).json({ message: error });
- }
- 
-   });
+    let post = await Posts.findById(req.params.postId).exec()
+    res.status(200).json(post)
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+
+});
 
 // POST new message.
 exports.create_post = [
@@ -49,8 +49,8 @@ exports.create_post = [
       });
       return;
     }
-  
-    
+
+
     const post = new Posts({
       title: req.body.title,
       text: req.body.text,
@@ -65,7 +65,7 @@ exports.create_post = [
       res.status(500).json({ message: error });
     }
 
-     }
+  }
 ]
 
 
@@ -75,70 +75,7 @@ exports.delete_post = asyncHandler(async (req, res, next) => {
 
   try {
     await Posts.findByIdAndDelete(req.params.postId);
-    res.status(200).json({deleted: req.params.postId})
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-
-     });
-
-     
-
-// PUT edit message
-
-exports.edit_post = asyncHandler(async (req, res, next) => {
-
-  
-    let postData = await Posts.findById({_id: req.params.postId});
-    
-  
-
-  if (postData.published == true) {
-
-  const post = new Posts({
-    title: postData.title,
-    text: postData.text,
-    published: false,
-    _id: req.params.postId
-   
-     });
-
-     try {
-      await Posts.findByIdAndUpdate(req.params.postId, post, {});
-      res.status(200).json({updated: req.params.postId})
-    } catch (error) {
-      res.status(500).json({ message: error });
-    }
-  }
-
-  if (postData.published == false) {
-
-    const post = new Posts({
-      title: postData.title,
-      text: postData.text,
-      published: true,
-      _id: req.params.postId
-     
-       });
-  
-       try {
-        await Posts.findByIdAndUpdate(req.params.postId, post, {});
-        res.status(200).json({updated: req.params.postId})
-      } catch (error) {
-        res.status(500).json({ message: error });
-      }
-    }
-     
-
-    })
-
-//delete comment
-
-exports.comments_delete = asyncHandler(async (req, res, next) => {
-  
-  try {
-    await Comments.findByIdAndDelete(req.params.commentId);
-    res.status(200).json({deleted: req.params.commentId})
+    res.status(200).json({ deleted: req.params.postId })
   } catch (error) {
     res.status(500).json({ message: error });
   }
@@ -147,4 +84,67 @@ exports.comments_delete = asyncHandler(async (req, res, next) => {
 
 
 
-    
+// PUT publish unpublish
+
+exports.edit_post = asyncHandler(async (req, res, next) => {
+
+
+    let postData = await Posts.findById({ _id: req.params.postId });
+
+
+
+    if (postData.published == true) {
+
+      const post = new Posts({
+        title: postData.title,
+        text: postData.text,
+        published: false,
+        _id: req.params.postId
+
+      });
+
+      try {
+        await Posts.findByIdAndUpdate(req.params.postId, post, {});
+        res.status(200).json({ updated: req.params.postId })
+      } catch (error) {
+        res.status(500).json({ message: error });
+      }
+    }
+
+    if (postData.published == false) {
+
+      const post = new Posts({
+        title: postData.title,
+        text: postData.text,
+        published: true,
+        _id: req.params.postId
+
+      });
+
+      try {
+        await Posts.findByIdAndUpdate(req.params.postId, post, {});
+        res.status(200).json({ updated: req.params.postId })
+      } catch (error) {
+        res.status(500).json({ message: error });
+      }
+    }
+
+
+  })
+
+
+//delete comment
+
+exports.comments_delete = asyncHandler(async (req, res, next) => {
+
+  try {
+    await Comments.findByIdAndDelete(req.params.commentId);
+    res.status(200).json({ deleted: req.params.commentId })
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+
+});
+
+
+
