e38b0653cdc60ab7c69a1de734783293c41def2c
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-01 19:29:04 -0500

    added new cors url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://blog-public-two.vercel.app/",
     credentials: true,
   })
 );
